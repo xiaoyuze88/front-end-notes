1267d22dccc48751bfbf757c1bfd5c7f78699c5e
Xiao Yuze <156260767@qq.com>
2023-04-21 02:20:44 +0800

    commit

--- src/leetcode/regular-expression-matching.js
+++ src/leetcode/regular-expression-matching.js
@@ -31,43 +31,35 @@
 // 保证每次出现字符 * 时，前面都匹配到有效的字符
 
 // 动态规划
-//
-function isMatch(s, p) {
-  const dp = [...Array(s.length + 1)].map(() => Array(p.length + 1).fill(false));
+// https://leetcode.cn/problems/regular-expression-matching/solution/da-bai-hua-dong-tai-gui-hua-dp-by-yu-ze-4h9ny/
+function isMatch(s: string, p: string): boolean {
+  const dp: boolean[][] = [...Array(s.length + 1)].map(() => Array(p.length + 1).fill(false));
 
+  // 都为空，必定不匹配
   dp[0][0] = true;
 
+  // s == 0, p 只有最后一位*才有可能匹配
   for (let j = 1, l = p.length + 1; j < l; j++) {
     if (p[j - 1] === "*") dp[0][j] = dp[0][j - 2];
   }
-  debugger;
+
   for (let i = 1, l_i = s.length + 1; i < l_i; i++) {
     for (let j = 1, l_j = p.length + 1; j < l_j; j++) {
-      if (i == 1 && j ==5 ) debugger
+      // 最后一位匹配
       if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
-
-
         dp[i][j] = dp[i - 1][j - 1];
       } else if (p[j - 1] === "*") {
+        // 重复1+次，或者重复0次的第二个case，由于条件一样，这里把结果合并
         if (p[j - 2] == s[i - 1] || p[j - 2] === ".") {
-          // 2、7 == 1、5失败，期望2、7==true但是1、5==false
-          // 1、5 == 0、5失效，期望1、5==true但是0、5==false，该条件有异常
-          dp[i][j] = dp[i - 1][j];
-        } else if (p[j - 2] !== s[i - 1]) {
+          dp[i][j] = dp[i - 1][j] || dp[i][j - 2];
+        }
+        // 重复0次的第一个case
+        else if (p[j - 2] !== s[i - 1]) {
           dp[i][j] = dp[i][j - 2];
         }
       }
     }
   }
 
-  // console.log(dp);
-
   return dp[s.length][p.length];
-}
-
-// console.log(isMatch("aa", "a"));
-// console.log(isMatch("aa", "a*"));
-// console.log(isMatch("ab", ".*"));
-// console.log(isMatch("c", "c"));
-// 条件遍历到 i= 2,j=7时不符合预期，此时 dp[2][7] == dp[2][5]   但是25==false，期望是true
-console.log(isMatch("aaa", "ab*a*c*a"));
+};
